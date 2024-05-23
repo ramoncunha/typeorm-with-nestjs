dc1535c59ae93dad1a35cbee13728d8219d490f0
Ramon Cunha <ramon.network@hotmail.com>
2024-05-22 23:18:26 -0300

    config para deletar app a cada start

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, unlinkSync } from 'fs';
 
 async function bootstrap() {
+  const dbFile = 'db.sqlite';
+  if (existsSync(dbFile)) unlinkSync(dbFile);
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
